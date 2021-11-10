168795bfcf75b66c566bc32ca94d31d6ca689063
bnguyen4 <bnguyen4@illinois.edu>
2021-11-09 22:38:48 -0600

    remove

--- parsing-real/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "include/node.h"
-
-int main() {
-    Node<int> n(5);
-    Node<int> n(6);
-
-    std::cout << n.getID() << ", " << n.getData()
-}
--- parsing-real/tests/test.cpp
+++
@@ -1,4 +0,0 @@
-#define CATCH_CONFIG_MAIN
-#include "catch2/catch.hpp"
-
-// https://github.com/catchorg/Catch2/blob/devel/docs/own-main.md#top
